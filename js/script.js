a61bf635bdecce8f4ddba9980ff7f733b41aabe9
Aleksey Belchenkov <u608110@gmail.com>
2020-04-23 13:34:05 +0300

    Calculate Wealth

--- js/script.js
+++ js/script.js
@@ -49,6 +49,14 @@ function showMillionaires() {
     updateDOM();
 }
 
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
 function addData(obj) {
     data.push(obj);
 
@@ -78,3 +86,4 @@ addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
 showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
